988075b68f0c02c4380d658eae391abd05f5407e
rldcarvalho <rldcarvalhoo@gmail.com>
2023-07-12 20:46:07 -0300

    Add components
    Home, films, starships and header components.
    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FilmsComponent } from './components/pages/films/films.component';
+import { StarshipsComponent } from './components/pages/starships/starships.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FilmsComponent,
+    StarshipsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
