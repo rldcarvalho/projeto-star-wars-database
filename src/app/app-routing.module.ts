f09ec6571bf39b683ab5ce6bccd0c5646aae7196
rldcarvalho <rldcarvalhoo@gmail.com>
2023-07-12 21:33:14 -0300

    Add basic menu toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { FilmsComponent } from './components/pages/films/films.component';
+import { StarshipsComponent } from './components/pages/starships/starships.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'films', component: FilmsComponent},
+  { path: 'starships', component: StarshipsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './components/pages/home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FilmsComponent } from './components/pages/films/films.component';
@@ -20,7 +22,9 @@ import { StarshipsComponent } from './components/pages/starships/starships.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
